3409fb5e0a65ab0b3a25590deac3a3fdf334b1eb
rashadat1 <rashadat@yahoo.com>
2024-11-11 10:24:50 -0500

    notification model

--- springBootBankProject/financeinstitution/src/main/java/com/example/financeinstitution/model/Notifications.java
+++ springBootBankProject/financeinstitution/src/main/java/com/example/financeinstitution/model/Notifications.java
@@ -1,5 +1,61 @@
 package com.example.financeinstitution.model;
+import java.time.LocalDateTime;
 
 public class Notifications {
-    
+    private Long id;
+    private Long user_id;
+    private String message;
+    private String notification_type;
+    private LocalDateTime timestamp;
+
+    public Notifications() {
+
+    }
+    public Notifications(Long id, Long user_id, String message, String notification_type, LocalDateTime timestamp) {
+        this.id = id;
+        this.user_id = user_id;
+        this.message = message;
+        this.notification_type = notification_type;
+        this.timestamp = timestamp;
+    }
+    public Long getId() {
+        return this.id;
+    }
+    public Long getUserId() {
+        return this.user_id;
+    }
+    public String getMessage() {
+        return this.message;
+    }
+    public String getNotificationType() {
+        return this.notification_type;
+    }
+    public LocalDateTime getTimestamp() {
+        return this.timestamp;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setUserId(Long user_id) {
+        this.user_id = user_id;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    public void setNotificationType(String notification_type) {
+        this.notification_type = notification_type;
+    }
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+    @Override
+    public String toString() {
+        return "Notification{" +
+                "id=" + this.id +
+                ", userId=" + this.user_id +
+                ", message='" + this.message + '\'' +
+                ", notificationType='" + this.notification_type + '\'' +
+                ", timestamp=" + this.timestamp +
+                '}';
+    }
 }
